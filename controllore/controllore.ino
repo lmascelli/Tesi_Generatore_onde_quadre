a56a7ad4a330ecbabad810d1dd24aed5201c7be8
lmascelli <lmascelli@gmail.com>
2021-08-11 11:05:57 +0200

    Forse completata la gestione degli stati. Prossimo passo il render del display.

--- controllore/controllore.ino
+++ controllore/controllore.ino
@@ -18,9 +18,19 @@
 #define DUTY_CYCLE 1
 #define REPETITIONS 2
 
+
+// #define I2C
+#ifdef I2C
+// #include <LiquidCrystal_I2C.h>
+// LiquidCrystal_I2C lcd(0x27, 20, 4);
+#else
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(0, 0, 0, 0, 0, 0);
+#endif
+
 float frequency, period, period_on;// Hertz
 float duty_cycle;
-unsigned int repetitions;
+float repetitions;
 double start_time, current_time;
 
 unsigned int decimals = 0, units = 0, tens  = 0, hundreds = 0;
@@ -29,14 +39,17 @@ bool buttons[4] = {false};
 enum STATE {
   PLAY,
   STOP,
+  MENU_FREQ,
   MENU_FREQ_0,
   MENU_FREQ_1,
   MENU_FREQ_2,
   MENU_FREQ_3,
+  MENU_DUTY_CYCLE,
   MENU_DUTY_CYCLE_0,
   MENU_DUTY_CYCLE_1,
   MENU_DUTY_CYCLE_2,
   MENU_DUTY_CYCLE_3,
+  MENU_REPETITIONS,
   MENU_REPETITIONS_0,
   MENU_REPETITIONS_1,
   MENU_REPETITIONS_2,
@@ -46,16 +59,10 @@ enum STATE {
 
 enum STATE current_state = STOP;
 
-struct change_digit_info_struct {
-  STATE prev;
-  STATE next;
-  unsigned int *digit;
-} change_digit_info;
 
-
-void set_digits(float num_to_convert){
+void set_digits(float num_to_convert) {
   if (num_to_convert >= 999.9) return;
-  int num = (int)(num_to_convert*10);
+  int num = (int)(num_to_convert * 10);
   hundreds = num % 1000;
   num = num / 10;
   tens = num % 10;
@@ -65,31 +72,29 @@ void set_digits(float num_to_convert){
   decimals = num % 10;
 }
 
-inline float create_from_digits(){
-  return 100*hundreds + 10*tens + units + 0.1f*decimals;
+inline void create_from_digits(float *num) {
+  *num = 100 * hundreds + 10 * tens + units + 0.1f * decimals;
 }
 
-void set_num(MENU_FLAG num){
-  switch (num) {
-    case FREQUENCY:
-      frequency = create_from_digits();
-      break;
-    case DUTY_CYCLE:
-      duty_cycle = create_from_digits();
-      break;
-    case REPETITIONS:
-      repetitions = create_from_digits();
-    default:
-      break;
-  }
-}
-
-inline bool compare_buttons(bool b0, bool b1, bool b2, bool b3){
+inline bool compare_buttons(bool b0, bool b1, bool b2, bool b3) {
   return (buttons[0] == b0 && buttons[1] == b2 && buttons[2] == b2 && buttons[3] == b3);
 }
 
-inline void change_digit(STATE calling, STATE next, int *num){
-  
+inline void change_digit(unsigned int *num, bool decrease) {
+  if (!decrease) {
+    if (*num == 9) *num = 0;
+    else *num++;
+  }
+  else {
+    if (*num == 0) *num = 9;
+    else *num--;
+  }
+  lcd.clear();
+}
+
+inline void change_state(STATE state) {
+  lcd.clear();
+  current_state = state;
 }
 
 void setup() {
@@ -98,9 +103,10 @@ void setup() {
   duty_cycle = 0.5f;
   period_on = period * duty_cycle;
   repetitions = 10;
+  lcd.begin(20, 4);
   start_time = GET_TIME();
 
-  
+
   pinMode(OUTPUT_PIN, OUTPUT);
   pinMode(PIN_RIGHT, INPUT);
   digitalWrite(OUTPUT_PIN, LOW);
@@ -127,67 +133,250 @@ void loop() {
   // manage state
   switch (current_state) {
     case STOP:
-    {
-      if (compare_buttons(false, false, false, true)){
-        current_state = PLAY;
-      } else if (compare_buttons(true, false, false, false)){
-        current_state = MENU_FREQ_0;
+      {
+        if (compare_buttons(false, false, false, true)) {
+          change_state(PLAY);
+        } else if (compare_buttons(true, false, false, false)) {
+          change_state(MENU_FREQ);
+        }
+        break;
       }
-      break;
-    }
     case PLAY:
-    {
-      unsigned int loop_repetitions = repetitions;
-      while (loop_repetitions > 0) {
-        current_time = GET_TIME();
-        double elapsed_time = current_time - start_time;
-        if (elapsed_time < period_on)
-          digitalWrite(OUTPUT_PIN, HIGH);
-        else if (elapsed_time < period)
-          digitalWrite(OUTPUT_PIN, LOW);
-        else {
-          start_time = GET_TIME();
-          loop_repetitions--;
+      {
+        float loop_repetitions = repetitions;
+        while (loop_repetitions > 0) {
+          current_time = GET_TIME();
+          double elapsed_time = current_time - start_time;
+          if (elapsed_time < period_on)
+            digitalWrite(OUTPUT_PIN, HIGH);
+          else if (elapsed_time < period)
+            digitalWrite(OUTPUT_PIN, LOW);
+          else {
+            start_time = GET_TIME();
+            loop_repetitions--;
+          }
         }
+        change_state(STOP);
+        break;
       }
-      current_state = STOP;
-      break;
-    }
-    case CHANGE_DIGIT:
-    {
-      if (compare_buttons(true, false, false, false)){
-        if (*(change_digit_info.digit) == 9) *(change_digit_info.digit) = 0;
-        else (*(change_digit_info.digit))++;
+    case MENU_FREQ:
+      {
+        set_digits(frequency);
+        if (compare_buttons(true, false, false, false))
+          change_state(STOP);
+        else if (compare_buttons(false, true, false, false))
+          change_state(MENU_DUTY_CYCLE);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_FREQ_0);
+        break;
       }
-      else if (compare_buttons(false, true, false, false)){
-        if (*(change_digit_info.digit) == 0) *(change_digit_info.digit) = 9;
-        else (*(change_digit_info.digit))--;
-      }
-      else if (compare_buttons(false, false, true, false))
-        current_state = change_digit_info.prev;
-      else if (compare_buttons(false, false, false, true))
-        current_state = change_digit_info.next;
-      break;
-    }
     case MENU_FREQ_0:
-    {
-      if (compare_buttons(true, false, false, false))
-        current_state = STOP;
-      else if (compare_buttons(false, true, false, false))
-        current_state = MENU_DUTY_CYCLE_0;
-      else if (compare_buttons(false, false, false, true))
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&hundreds, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&hundreds, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_FREQ);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_FREQ_1);
+        break;
+      }
+    case MENU_FREQ_1:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&tens, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&tens, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_FREQ_0);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_FREQ_2);
+        break;
+      }
+    case MENU_FREQ_2:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&units, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&units, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_FREQ_1);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_FREQ_2);
+        break;
+      }
+    case MENU_FREQ_3:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&decimals, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&decimals, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_FREQ_2);
+        else if (compare_buttons(false, false, false, true))
         {
-          change_digit_info.prev = STOP;
-          change_digit_info.next = MENU_FREQ_0;
-          change_digit_info.digit = &hundreds; 
-          current_state = CHANGE_DIGIT;
+          change_state(MENU_FREQ);
+          create_from_digits(&frequency);
         }
+        break;
+      }
+    case MENU_DUTY_CYCLE:
+      {
+        set_digits(duty_cycle);
+        if (compare_buttons(true, false, false, false))
+          change_state(MENU_FREQ);
+        else if (compare_buttons(false, true, false, false))
+          change_state(MENU_REPETITIONS);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_DUTY_CYCLE_0);
+        break;
+      }
+    case MENU_DUTY_CYCLE_0:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&hundreds, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&hundreds, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_DUTY_CYCLE);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_DUTY_CYCLE_1);
+        break;
+      }
+    case MENU_DUTY_CYCLE_1:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&tens, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&tens, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_DUTY_CYCLE_0);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_DUTY_CYCLE_1);
+        break;
+      }
+    case MENU_DUTY_CYCLE_2:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&units, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&units, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_DUTY_CYCLE_1);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_DUTY_CYCLE_3);
+        break;
+      }
+    case MENU_DUTY_CYCLE_3:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&decimals, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&decimals, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_DUTY_CYCLE_2);
+        else if (compare_buttons(false, false, false, true))
+        {
+          change_state(MENU_DUTY_CYCLE);
+          create_from_digits(&duty_cycle);
+        }
+        break;
+      }
+    case MENU_REPETITIONS:
+      {
+        set_digits(repetitions);
+        if (compare_buttons(true, false, false, false))
+          change_state(MENU_DUTY_CYCLE);
+        else if (compare_buttons(false, true, false, false))
+          change_state(STOP);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_REPETITIONS_0);
+        break;
+      }
+    case MENU_REPETITIONS_0:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&hundreds, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&hundreds, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_REPETITIONS);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_REPETITIONS_1);
+        break;
+      }
+    case MENU_REPETITIONS_1:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&tens, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&tens, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_REPETITIONS_0);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_REPETITIONS_2);
+        break;
+      }
+    case MENU_REPETITIONS_2:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&units, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&units, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_REPETITIONS_1);
+        else if (compare_buttons(false, false, false, true))
+          change_state(MENU_REPETITIONS_3);
+        break;
+      }
+    case MENU_REPETITIONS_3:
+      {
+        if (compare_buttons(true, false, false, false))
+          change_digit(&decimals, false);
+        else if (compare_buttons(false, true, false, false))
+          change_digit(&decimals, true);
+        else if (compare_buttons(false, false, true, false))
+          change_state(MENU_REPETITIONS_2);
+        else if (compare_buttons(false, false, false, true))
+        {
+          change_state(MENU_REPETITIONS);
+          create_from_digits(&repetitions);
+        }
+        break;
+      }
+    default:
       break;
-    }
   }
 
   // render to display
   {
-    
+    switch (current_state) {
+      case STOP:
+        {
+          lcd.print("STOP");
+          break;
+        }
+      case PLAY:
+        {
+          lcd.print("ACTIVE");
+          break;
+        }
+      case MENU_FREQ_0:
+      case MENU_FREQ_1:
+      case MENU_FREQ_2:
+      case MENU_FREQ_3:
+        {
+          lcd.print("Frequency");
+          break;
+        }
+      case CHANGE_DIGIT:
+        {
+          lcd.print("Change Digit");
+          break;
+        }
+      default:
+        break;
+    }
   }
 }
